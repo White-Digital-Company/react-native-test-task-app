26a1e2451e4b47295a7c2c81ab0ac62ae2a3c4e6
Volodymyr Bondarets <bondaretsdirect@gmail.com>
2024-08-02 01:42:54 +0300

    feat: Added headers and button to screens

--- src/screens/ActivityScreen/ActivityScreen.tsx
+++ src/screens/ActivityScreen/ActivityScreen.tsx
@@ -1,14 +1,21 @@
 import { Text, View } from 'react-native'
 import Header from '../../components/header/Header.component'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { text } from '../../shared/text/text'
+import DefaultButton from '../../components/button/default-button.component'
 
 const ActivityScreen = () => {
   return (
-    <SafeAreaView className="flex-1 items-center flex-col bg-blue-200">
+    <SafeAreaView className="flex-1 items-center flex-col bg-blue-200 px-6">
       <Header isGoBack />
-      <View className="flex-1 items-center justify-center">
+      <View className="flex-1 items-center justify-center w-full">
         <Text>ActivityScreen</Text>
       </View>
+      <DefaultButton onPress={() => {}}>
+        <Text className="text-white font-normal text-base font-[abel]">
+          {text.addToFav}
+        </Text>
+      </DefaultButton>
     </SafeAreaView>
   )
 }
