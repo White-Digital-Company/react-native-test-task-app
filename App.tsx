a1d8f637f9307ab5d04751f82fd3e7e058ace224
YuriiStetsula <s.yuri@ukr.net>
2024-08-02 18:40:28 +0300

    feat: add tanstack / axios

--- App.tsx
+++ App.tsx
@@ -3,12 +3,15 @@ import RootRouter from './src/screens/RootRouter'
 import { StatusBar } from 'react-native'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 const Root = () => {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <RootRouter />
-    </>
+    </QueryClientProvider>
   )
 }
 
