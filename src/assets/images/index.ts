fa685f39064e727afa67ed7a84ee9b7c7248d587
YuriiStetsula <s.yuri@ukr.net>
2024-08-02 13:42:47 +0300

    build: add assets

---
+++ src/assets/images/index.ts
@@ -0,0 +1,4 @@
+import { ImageSourcePropType } from 'react-native'
+
+export const starImage: ImageSourcePropType = require('./Star.png')
+export const pinImage: ImageSourcePropType = require('./MapPin.png')
