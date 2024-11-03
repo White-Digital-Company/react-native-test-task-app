41de26bf6daf4486192130c9bb6d4822b0361b29
olena.slavinska <slavinskaya.lena2001@gmail.com>
2024-11-03 18:03:27 +0200

    set up project

--- App.tsx
+++ App.tsx
@@ -1,17 +1,16 @@
-import '@/i18n'
-import RootRouter from '@/screens/RootRouter'
-import { NavigationContainer } from '@react-navigation/native'
-import { StatusBar } from 'react-native'
-import { GestureHandlerRootView } from 'react-native-gesture-handler'
-import { SafeAreaProvider } from 'react-native-safe-area-context'
+import RootRouter from '@/screens/RootRouter';
+import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const Root = () => {
   return (
     <>
       <RootRouter />
     </>
-  )
-}
+  );
+};
 
 const App = () => {
   return (
@@ -23,7 +22,7 @@ const App = () => {
         </NavigationContainer>
       </GestureHandlerRootView>
     </SafeAreaProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
