d4591ee3d3589aa0d2ce7044e081f97e27c3747e
fronik <askolya23@gmail.com>
2024-11-08 18:09:12 +0200

    [+] - install dependencies and create new screen

---
+++ src/screens/DetailsScreen.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const DetailsScreen = () => {
+  return (
+    <View>
+      <Text>Hello, developer!</Text>
+    </View>
+  )
+}
+
+export default DetailsScreen
